4be8db2335cea8b761ffc9bb8d8d18339188a51a
Dhruv Modi <>
2023-01-16 22:45:09 +0530

    done

--- script.js
+++ script.js
@@ -44,14 +44,18 @@ function setboardsize(i){
     localStorage.setItem('boardSize',i);
 }
 try {
-    
+    var img = document.createElement('img');
+    img.src = "images/nqueen.png";
+    img.width = "10%";
 for(var i =0;i<boardSize;i++){
     board.appendChild(document.createElement('div'));
     var w = board.offsetWidth/boardSize;
     for (var j = 0; j < boardSize; j++) {
         var color = (i+j)%2==1?"teal":"white";
-        board.children[i].appendChild(document.createElement('div'));
-        board.children[i].children[j].innerHTML = i+","+j;
+        var box = document.createElement('div');
+        box.id = i*boardSize+j;
+        board.children[i].appendChild(box);
+        // board.children[i].children[j].appendChild(img);
         board.children[i].children[j].setAttribute(`style`,
         `
         width:${w}px;
@@ -59,7 +63,6 @@ for(var i =0;i<boardSize;i++){
         `
         );
     }
-    console.log(board.children[i].children)
     board.children[i].setAttribute('style',
     `display:flex;
     `
@@ -70,3 +73,36 @@ for(var i =0;i<boardSize;i++){
     
 }
 
+var nQueen = new Array(boardSize);
+for(var i =0;i<boardSize;i++){
+    nQueen[i] = new Array(boardSize);
+    for (var j = 0; j < boardSize; j++){
+        nQueen[i][j] = false;
+    }
+}
+try {
+    board.addEventListener('click',(e)=>{
+        var id = e.target.id;
+        var box = document.getElementById(id);
+        if(id[0]=='i'){
+            id = id.slice(3,id.length);
+        }
+        var i = Math.floor(id/boardSize);
+        var j = id-(i)*boardSize;
+        var img = `<img src=images/nqueen.png width=1% id=img${id}>`;
+
+        for(var k =0;k<boardSize;k++){
+            if(j!=k)
+            if(board.children[i].children[k].innerHTML!="") board.children[i].children[k].innerHTML="";
+        }
+        if(board.children[i].children[j].innerHTML==""){
+            board.children[i].children[j].innerHTML=img;
+        }
+        else{
+            board.children[i].children[j].innerHTML="";
+    }
+    });
+    
+} catch (error) {
+    
+}
